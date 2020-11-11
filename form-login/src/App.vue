b8eb87ddcd2bb58e46148ae1ca53e6b345e55e80
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2020-11-11 14:23:50 -0300

    Commit inicial

---
+++ form-login/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <FormLogin/>
+  </div>
+</template>
+
+<script>
+
+import FormLogin from "@/components/login/FormLogin";
+export default {
+  name: 'App',
+  components: {FormLogin}
+}
+</script>
+
+<style>
+*, html, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #eaeaea;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ form-login/src/components/login/FormLogin.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="form-container">
+    <form>
+      
+      <input type="email" id="email" name="email" placeholder="Email or Phone Number">
+      <input type="password" id="password" name="password" placeholder="Password">
+
+      <button class="btn">Log In</button>
+
+      <div class="forget-password">
+        <a href="#">Forgot Password?</a>  
+      </div>
+      <div class="divider">
+      </div>
+      <button>Create New Account</button>  
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FormLogin"
+}
+</script>
+
+<style scoped>
+
+form {
+  width: 395px;
+  padding: 15px;
+  background: #fff;
+}
+
+input {
+  padding: 14px 16px;
+  font-size: 17px;
+  width: 100%;
+  display: block;
+  margin-bottom: 12px;
+}
+
+.btn {
+  background: #166fe5;
+  color: #fff;
+  border: none;
+  border-radius: 6px;
+  line-height: 48px;
+  width: 100%;
+  display: block;
+  margin-bottom: 24px;
+  font-weight: bold;
+  font-size: 20px;
+
+}
+
+.forget-password {
+  margin-bottom: 24px;
+}
+a {
+  text-align: center;
+  text-decoration: none;
+  color: #166fe5;
+}
+
+.divider {
+  width: 100%;
+  margin-bottom: 24px;
+  background: blue;
+  border-bottom: 1px solid #dadde1;
+}
+</style>
